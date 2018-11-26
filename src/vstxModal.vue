50374a72023ab8c60b2b149e4927899dd2fce344
Jeremy R DeYoung <Jeremy@NextStepGuru.com>
2018-11-26 12:24:09 -0600

    added modal-card support

--- src/vstxModal.vue
+++ src/vstxModal.vue
@@ -1,16 +1,45 @@
 <template lang="pug">
 .modal(:class="{ 'is-active': isVisible }")
-  .modal-background(@click.passive="toggleIsVisible")
-  .modal-content.box.is-scrollable-y 
-    slot
-  button.modal-close.is-large(@click.passive="toggleIsVisible")
+  template(v-if="modalType === 'default")
+    .modal-background(@click.passive="toggleIsVisible")
+    .modal-content.box.is-scrollable-y
+      slot
+    button.modal-close.is-large(@click.passive="toggleIsVisible")
+  template(v-else-if="modalType === 'card")
+    .modal-background(@click.passive="toggleIsVisible")
+    .modal-card
+      header.modal-card.head
+        slot(name="header")
+          p.modal-card-title {{ title }}
+          button.delete(aria-label="close", @click.passive="toggleIsVisible")
+      section.modal-card-body
+        slot(name="body")
+      footer.modal-card-foot
+        slot(name="footer")
+          button.button-is-success(@click.passive="$emit('modalSaveChanges')") Save Changes
+          button.button(@click.passive="toggleIsVisible") Cancel
 </template>
 
 <script>
 export default {
   name: 'VModal',
   props: {
-    isVisible: Boolean
+    modalType: {
+      type: String,
+      required: false,
+      default: 'default'
+      // modalType: Card would render the Bulma Card Modal
+    },
+    isVisible: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    }
   },
   data () {
     return {
